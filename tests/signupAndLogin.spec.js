f8995b0e92aa5cee037b42362a1b8e4265e2ada2
Faceofspit <krishnachitragar0@gmail.com>
2025-09-20 14:07:44 +0530

    Login related code

---
+++ tests/signupAndLogin.spec.js
@@ -0,0 +1,62 @@
+import { test, expect } from "@playwright/test"
+import { signup } from "Pages/signup"
+
+
+test("Sign up to the Real", async({page})=>{
+
+    const real = new signup(this.page);
+
+   await  real.Navigate();
+   await  real.joinReal();
+   await  real.SignUp();    
+})
+
+test("Sign up to the Real Negative Testcase with wrong email", async({page})=>{
+
+    const real = new signup(this.page);
+
+   await  real.Navigate();
+   await  real.joinReal();
+   await  real.SignUpEmailError();    
+})
+
+test("Sign up to the Real Negative Testcase with wrong password", async({page})=>{
+
+    const real = new signup(this.page);
+
+   await  real.Navigate();
+   await  real.joinReal();
+   await  real.signupWrongPassword();    
+})
+
+
+
+test("Testcase 1 with all dollars", async ({ page }) => {
+
+const real = new signup(this.page);
+
+    await real.Login();
+    await real.transactionEyeIcon();
+    await real.deatilsAdjustTestcase1();
+    
+})
+
+test("Testcase 2 with two dollars and two percentages", async ({ page }) => {
+
+const real = new signup(this.page);
+
+    await real.Login();
+    await real.transactionEyeIcon();
+    await real.deatilsAdjustTestcase2();
+    
+})
+
+test("Testcase 3 with all 4 percentages", async ({ page }) => {
+
+const real = new signup(this.page);
+
+    await real.Login();
+    await real.transactionEyeIcon();
+    await real.deatilsAdjustTestcase3();
+    
+})
