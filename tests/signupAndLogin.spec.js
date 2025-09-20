d809017eaf572108056f1dcd4ed46eeca65aa1e1
Faceofspit <krishnachitragar0@gmail.com>
2025-09-20 18:01:20 +0530

    Login related code

--- tests/signupAndLogin.spec.js
+++ tests/signupAndLogin.spec.js
@@ -2,61 +2,64 @@ import { test, expect } from "@playwright/test"
 import { signup } from "../pages/signup"
 
 
-test("Sign up to the Real", async({page})=>{
+test("Sign up to the Real", async ({ page }) => {
 
     const real = new signup(page);
 
-   await  real.Navigate();
-   await  real.joinReal();
-   await  real.SignUp();    
+    await real.Navigate();
+    await real.joinReal();
+    await real.SignUp();
 })
 
-test("Sign up to the Real Negative Testcase with wrong email", async({page})=>{
+test("Sign up to the Real Negative Testcase with wrong email", async ({ page }) => {
 
     const real = new signup(page);
 
-   await  real.Navigate();
-   await  real.joinReal();
-   await  real.SignUpEmailError();    
+    await real.Navigate();
+    await real.joinReal();
+    await real.SignUpEmailError();
 })
 
-test("Sign up to the Real Negative Testcase with wrong password", async({page})=>{
+test("Sign up to the Real Negative Testcase with wrong password", async ({ page }) => {
 
     const real = new signup(page);
 
-   await  real.Navigate();
-   await  real.joinReal();
-   await  real.signupWrongPassword();    
+    await real.Navigate();
+    await real.joinReal();
+    await real.signupWrongPassword();
 })
 
 
 
 test("Testcase 1 with all dollars", async ({ page }) => {
 
-const real = new signup(page);
+    const real = new signup(page);
 
+    await real.Navigate();
     await real.Login();
     await real.transactionEyeIcon();
     await real.deatilsAdjustTestcase1();
-    
+
 })
 
 test("Testcase 2 with two dollars and two percentages", async ({ page }) => {
 
-const real = new signup(page);
+    const real = new signup(page);
 
+    await real.Navigate();
     await real.Login();
     await real.transactionEyeIcon();
     await real.deatilsAdjustTestcase2();
-    
+
 })
 
 test("Testcase 3 with all 4 percentages", async ({ page }) => {
 
-const real = new signup(page);
+    const real = new signup(page);
 
+    await real.Navigate();
     await real.Login();
     await real.transactionEyeIcon();
     await real.deatilsAdjustTestcase3();
-    
+
 })
